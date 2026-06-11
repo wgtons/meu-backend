da2978b7c49c8161dd7fed2a4d558465ca090d3d
wgtons <wellington.silva1725@gmail.com>
2026-06-11 14:13:40 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const dns = require('dns').promises;
 const path = require('path');
 const app = express();
+app.use(cors());
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
